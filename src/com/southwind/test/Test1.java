aed631cca029bc83f6d4b5ef8aabd83227e52a2e
shi <Administrator@192.168.0.103>
2019-12-28 22:45:23 +0800

    第一次提交

---
+++ src/com/southwind/test/Test1.java
@@ -0,0 +1,19 @@
+package com.southwind.test;
+
+public class Test1 extends ClassLoader{
+	public static void main(String[] args) {
+		System.out.println("hello GitHub");
+	}
+
+	public Test1() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Test1(ClassLoader parent) {
+		super(parent);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+}
